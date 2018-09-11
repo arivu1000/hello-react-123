2f861f716a36cfbcd8136d8272f57e162e8a5a44
Jabadurai Zachariah <arivu10000@gmail.com>
2018-09-11 13:05:52 +0530

    New Component CReated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Header from "./Header";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <Header text="Hello Coder" />
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
